dc48a56462e1cef8fd37b7484c3c9517f3d047a8
zhongjingwen <lisazjw.zhong@mail.utoronto.ca>
2024-05-30 10:13:35 -0400

    format code with Flake8

--- Hypsography/compute_hypsometry.py
+++ Hypsography/compute_hypsometry.py
@@ -1,27 +1,23 @@
-# # """ Calculate the hypsographic curve from latitude longitude
-# # registered elevation data, and save the resulting data as csv file
-# # using this format:
+""" Calculate the hypsographic curve from latitude longitude
+registered elevation data, and save the resulting data as csv file
+using this format:
 
-# # Elevation, CumSum
-# # -11000,    0
+Elevation, CumSum
+-11000,    0
 
-# # Use 30 minute resulution to test, see
-# # https://www.pygmt.org/dev/api/generated/pygmt.datasets.load_earth_relief.html
-# # for other resolutions
-# # """
+Use 30 minute resulution to test, see
+https://www.pygmt.org/dev/api/generated/pygmt.datasets.load_earth_relief.html
+for other resolutions
+"""
 
-import time
-import pygmt as gmt
-import pandas as pd
+
 import numpy as np
 import numpy.typing as npt
-import matplotlib.pyplot as plt
-from joblib import Parallel, delayed
-import multiprocessing
 from math import cos, sin, radians, pi
 
 # declare numpy float64 array type
 NDArrayFloat = npt.NDArray[np.float64]
+
 
 def earth_radius(
     lat: float,
@@ -42,7 +38,8 @@ def grid_area(
     lat: float,
     size: float,
 ) -> float:
-    """Calculate the area of a rectangular area of size = 1 deg at a given lat-long position"""
+    """Calculate the area of a rectangular area of size = 1 deg at a given
+        lat-long position"""
     r = earth_radius(lat) / 1000
     dy = (size * r * pi) / 180
     dx = size / 180 * pi * r * cos(radians(lat))
@@ -62,7 +59,8 @@ def slice_count(
     """Generate elevation count array for each latitudinal slice"""
     sub_grid = grid[start:end, ...]
 
-    count = np.zeros((elevation_maximum - elevation_minimum) // dz, dtype=float)
+    count = np.zeros((elevation_maximum - elevation_minimum) // dz,
+                     dtype=float)
 
     for i, e in enumerate(elevations):
         a = np.sum(np.logical_and(sub_grid > e, sub_grid < e + dz), axis=1)
@@ -86,5 +84,5 @@ def process_slice(
     lat_slice = lat[start:end]
     weight = np.array([grid_area(lat_val, dx) for lat_val in lat_slice])
     return slice_count(
-        start, end, weight, grid, elevation_minimum, elevation_maximum, elevations, dz
-    )
+        start, end, weight, grid, elevation_minimum, elevation_maximum,
+        elevations, dz)
